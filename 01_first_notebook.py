f82fe089b5bcf47448028d83b62373ce37c330ea
mythripartha <72578347+mythripartha@users.noreply.github.com>
2020-10-09 11:26:06 -0500

    Add files via upload

---
+++ 01_first_notebook.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Introduction, This is my *first* Jupyter Notebook
+
+# In[6]:
+
+
+x = 5
+y = 6
+z = x + y
+
+
+# In[7]:
+
+
+print(z)
+
+
+# In[ ]:
+
+
+
+

ee59f6cb9fb4f3c583ac70d9fc681cb70c2f6fc6
mythripartha <72578347+mythripartha@users.noreply.github.com>
2020-10-12 15:00:35 -0500

    Add files via upload

---
+++ 02_numpy_matplotlib.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import numpy as np
+
+
+# In[10]:
+
+
+m = 5
+c = -1
+X = np.array(x)
+Y = m*X + c
+
+
+# In[9]:
+
+
+print(Y)
+
+
+# In[8]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[12]:
+
+
+plt.scatter(X, Y)
+
